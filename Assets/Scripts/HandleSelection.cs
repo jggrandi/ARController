d1da602671b5a296718fcf21cb3102f2abfe15a1
Jeronimo Grandi <jggrandi@gmail.com>
2016-12-05 23:26:23 -0200

    Selection with mouse and touch

--- Assets/Scripts/HandleSelection.cs
+++ Assets/Scripts/HandleSelection.cs
@@ -20,15 +20,15 @@ public class HandleSelection : MonoBehaviour {
         Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
-            //Debug.Log(hit.transform.gameObject);
+			Debug.Log(Input.GetMouseButton(0));
             selected = hit.transform.gameObject ;
-            if(Input.touchCount <= 0)
+			if(Input.touchCount <= 0 || Input.GetMouseButton(0) == false)
                 selected.transform.GetComponent<Renderer>().material.color = Color.yellow;
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+			if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ||Input.GetMouseButton(0) == true) {
                 selected.transform.gameObject.transform.parent = selectedObject.transform;
                 selected.transform.GetComponent<Renderer>().material.color = Color.red;
             }
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButton(0) == false) {
                 selected.transform.gameObject.transform.parent = trackedObjects.transform;
             }
 
