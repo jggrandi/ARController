5edc3a5c54b8664e1f24b10323fceead28aabb3c
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2016-12-14 18:02:21 -0200

    Added function to calculate the average center of a group of objects

--- Assets/Scripts/HandleTransformations.cs
+++ Assets/Scripts/HandleTransformations.cs
@@ -52,33 +52,14 @@ namespace Lean.Touch {
         }
         protected virtual void OnEnable() {
             // Hook into the events we need
-            LeanTouch.OnFingerDown += OnFingerDown;
             LeanTouch.OnFingerSet += OnFingerSet;
-            LeanTouch.OnFingerUp += OnFingerUp;
-            LeanTouch.OnFingerTap += OnFingerTap;
-            LeanTouch.OnFingerSwipe += OnFingerSwipe;
-            LeanTouch.OnFingerHeldDown += OnFingerHeldDown;
-            LeanTouch.OnFingerHeldSet += OnFingerHeld;
-            LeanTouch.OnFingerHeldUp += OnFingerHeldUp;
-           // LeanTouch.OnGesture += OnGesture;
         }
 
         protected virtual void OnDisable() {
             // Unhook the events
-            LeanTouch.OnFingerDown -= OnFingerDown;
             LeanTouch.OnFingerSet -= OnFingerSet;
-            LeanTouch.OnFingerUp -= OnFingerUp;
-            LeanTouch.OnFingerTap -= OnFingerTap;
-            LeanTouch.OnFingerSwipe -= OnFingerSwipe;
-            LeanTouch.OnFingerHeldDown -= OnFingerHeldDown;
-            LeanTouch.OnFingerHeldSet -= OnFingerHeld;
-            LeanTouch.OnFingerHeldUp -= OnFingerHeldUp;
-           // LeanTouch.OnGesture -= OnGesture;
         }
 
-        public void OnFingerDown(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " began touching the screen");
-        }
 
         public void OnFingerSet(LeanFinger finger) {
             if (LeanTouch.Fingers.Count != 1) return;
@@ -91,86 +72,40 @@ namespace Lean.Touch {
 
                 }
             } else if (Mode == 1) {
-                Vector3 avg = Vector3.zero;
-                foreach (GameObject g in MainController.control.objSelectedNow) {
-                    avg += g.transform.position;
-                }
-                avg /= MainController.control.objSelectedNow.Count;
-                
+                Vector3 avg = avgCenterOfObjects(MainController.control.objSelectedNow);
                 Vector3 axis = Camera.main.transform.right * finger.ScreenDelta.y + Camera.main.transform.up * -finger.ScreenDelta.x;
 
                 foreach (GameObject g in MainController.control.objSelectedNow) {
-                    g.transform.RotateAround(avg, axis, finger.ScreenDelta.magnitude*0.1f);
+                    g.transform.RotateAround(avg, axis, finger.ScreenDelta.magnitude * 0.1f);
 
                 }
-
-
-
-                }
-            Debug.Log("Finger " + finger.Index + " is still touching the screen");
-        }
-
-        public void OnFingerUp(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " finished touching the screen");
-        }
-
-        public void OnFingerTap(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " tapped the screen");
-        }
-
-        public void OnFingerSwipe(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " swiped the screen");
-        }
-
-        public void OnFingerHeldDown(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " began touching the screen for a long time");
-        }
-
-        public void OnFingerHeld(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " is still touching the screen for a long time");
-        }
-
-        public void OnFingerHeldUp(LeanFinger finger) {
-            Debug.Log("Finger " + finger.Index + " stopped touching the screen for a long time");
+            }
         }
 
         public void OnGesture(List<LeanFinger> fingers) {
-            if (Mode == 0) {
+            if (Mode == (int)Utils.Transformations.Translation) {
 
-                Vector3 avg = Vector3.zero;
-                foreach (GameObject g in MainController.control.objSelectedNow) {
-                    avg += g.transform.position;
-                }
-                avg /= MainController.control.objSelectedNow.Count;
-
+                Vector3 avg = avgCenterOfObjects(MainController.control.objSelectedNow);
                 Vector3 dir = avg - Camera.main.transform.position;
 
                 foreach (GameObject g in MainController.control.objSelectedNow) {
                     g.transform.position += dir * LeanGesture.GetScreenDelta(fingers).y * 0.01f;
-                }
-            } else if (Mode == 1) {
 
-                Vector3 avg = Vector3.zero;
-                foreach (GameObject g in MainController.control.objSelectedNow) {
-                    avg += g.transform.position;
                 }
-                avg /= MainController.control.objSelectedNow.Count;
+            } else if (Mode == (int)Utils.Transformations.Rotation) {
+
+                Vector3 avg = avgCenterOfObjects(MainController.control.objSelectedNow);
 
                 float angle = LeanGesture.GetTwistDegrees(fingers);
-                //Vector3 delta = LeanGesture.GetScreenDelta(fingers);
                 Vector3 axis = Camera.main.transform.forward;
 
                 foreach (GameObject g in MainController.control.objSelectedNow) {
                     g.transform.RotateAround(avg, axis, angle);
                 }
 
-            } else if (Mode == 2) {
+            } else if (Mode == (int)Utils.Transformations.Scale) {
 
-                Vector3 avg = Vector3.zero;
-                foreach (GameObject g in MainController.control.objSelectedNow) {
-                    avg += g.transform.position;
-                }
-                avg /= MainController.control.objSelectedNow.Count;
+                Vector3 avg = avgCenterOfObjects(MainController.control.objSelectedNow);
 
                 float scale = LeanGesture.GetPinchScale(fingers);
 
@@ -181,20 +116,16 @@ namespace Lean.Touch {
 
             }
         }
-        void handleTranslation() {
 
+        private Vector3 avgCenterOfObjects(List<GameObject> objects) {
 
-        }
+            Vector3 avg = Vector3.zero;
+            foreach (GameObject g in objects) {
+                avg += g.transform.position;
+            }
 
-
-        void handleRotation() {
-
-
-        }
-
-        void handleScale() {
-
-        }
+            return avg /= objects.Count;
+        } 
 
         private void Scale(GameObject obj, float scale, Vector3 center) {
 
